c325c0eb2198bf7f3446cc8323ce6b3f96a21436
Jose Daniel Acuna <jodaniel1409@gmail.com>
2021-04-10 18:13:43 -0600

    [ADD] Models and services
    IDbManger and IFilePublisher interfaces
    File and Employee models

---
+++ Models/File.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DocumentAnalyzerService.Models
+{
+    public class File
+    {
+        public File(string fileName, List<KeyValuePair<string, int>> appearance)
+        {
+            FileName = fileName;
+            Appearance = appearance;
+        }
+
+        public string FileName { get; set; }
+
+        // List of employees (Name and appearance count)
+        public List<KeyValuePair<string, int>> Appearance { get; set; }
+    }
+}
---
+++ Services/IFilePublisher.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.Metadata;
+
+namespace DocumentAnalyzerService.Services
+{
+    // Uploads/downloads files to/from the Azure Object Storage
+    public interface IFilePublisher
+    {
+        public void UploadFile(Blob file);
+
+        public Blob GetFile(string fileName);
+    }
+}
